bc2f8078af5960a0f1778c4faf223fb2cdcef2c3
sudomichael <devmpk@gmail.com>
2016-11-09 15:37:49 -0500

    added base template and first block page, modified views.py to send movies to block template

--- nfs/views.py
+++ nfs/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from .models import Movie
 # Create your views here.
 
 def movie_list(request):
-    return render(request, 'nfs/movie_list.html', {})
+    movies = Movie.objects.filter(tomatoMeter__gte=90).order_by('-tomatoMeter')
+    return render(request, 'nfs/movie_list.html', {'movies': movies})
 
 

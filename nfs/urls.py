16dcfac9a00e31c4ca77ad2cbb95b1d576ea00cc
sudomichael <devmpk@gmail.com>
2016-11-11 16:53:29 -0500

    reformatted nav bar

--- nfs/urls.py
+++ nfs/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     url(r'^tomatosort$', views.movie_list_rt, name="movie_list_rt"),
     url(r'^imdbsort$', views.movie_list_imdb, name="movie_list_imdb"),
     url(r'^mcsort$', views.movie_list_mc, name="movie_list_mc"),
+    url(r'^netflixmovies/all$', views.movies_main_avg, name="movies_main_avg"),
+    url(r'^netflixmovies/tomatosort$', views.movies_main_rt, name="movies_main_rt"),
+   url(r'^netflixmovies/imdbsort$', views.movies_main_imdb, name="movies_main_imdb"),
+   url(r'^netflixmovies/mcsort$', views.movies_main_mc, name="movies_main_mc"),
 ]
--- nfs/views.py
+++ nfs/views.py
@@ -18,6 +18,8 @@ class pageInfo(object):
 
 homePageInfo = pageInfo('movie_list_rt', 'movie_list_imdb', 'movie_list_mc', 'movie_list', "Top Movies on Netflix Right Now")
 
+moviePageInfo = pageInfo('movies_main_rt', 'movies_main_imdb', 'movies_main_mc', 'movies_main_avg', 'Best Movies Airing on Netflix')
+
 def movie_list(request):
     movies = Movie.objects.filter(tomatoMeter__gte=1).filter(Metascore__gte=1).order_by('-average')[:50]
     return render(request, 'nfs/movie_list.html', {'movies': movies, 'pageInfo':homePageInfo})
@@ -37,3 +39,22 @@ def movie_list_mc(request):
 def movie_detail(request, pk):
     movie = get_object_or_404(Movie, pk=pk)
     return render(request, 'nfs/movie_detail.html', {'movie': movie})
+
+def movies_main_avg(request):
+    movies = Movie.objects.filter(tomatoMeter__gte=1).filter(Metascore__gte=1).filter(Type='movie').order_by('-average')[:50]
+    return render(request, 'nfs/movie_list.html', {'movies': movies, 'pageInfo':moviePageInfo})
+
+def movies_main_rt(request):
+    movies = Movie.objects.filter(tomatoMeter__gte=1).filter(Metascore__gte=1).filter(Type='movie').order_by('-tomatoMeter')[:50]
+    return render(request, 'nfs/movie_list.html', {'movies': movies, 'pageInfo':moviePageInfo})
+
+def movies_main_imdb(request):
+    movies = Movie.objects.filter(tomatoMeter__gte=1).filter(Metascore__gte=1).filter(Type='movie').order_by('-imdbRating')[:50]
+    return render(request, 'nfs/movie_list.html', {'movies': movies, 'pageInfo':moviePageInfo})
+
+def movies_main_mc(request):
+    movies = Movie.objects.filter(tomatoMeter__gte=1).filter(Metascore__gte=1).filter(Type='movie').order_by('-Metascore')[:50]
+    return render(request, 'nfs/movie_list.html', {'movies': movies, 'pageInfo':moviePageInfo})
+
+
+

969743264b87ffb78f66a54de8ad8b7119159cb2
sudomichael <devmpk@gmail.com>
2016-12-16 23:33:42 -0500

    more movies

--- dataLoad/apiRetrieval.py
+++ dataLoad/apiRetrieval.py
@@ -105,7 +105,7 @@ def changeAttributeTypes(movie):
         movie["average"] = 0
         pass
     try:
-        movie["kind"] = "usa"    
+        movie["kind"] = "canada"    
     except IndexError:
         pass
     return movie 
